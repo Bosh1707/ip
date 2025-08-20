03b824c21327ef450776cf136af046ee1c5e37ce
Joshua <joshua.rahul1707@gmail.com>
2025-08-21 03:14:42 +0800

    Level-2: Add tasks and list stored tasks (fixed-size array)

--- src/main/java/Bosh.java
+++ src/main/java/Bosh.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Bosh {
     private static final String line = "____________________________________________________________";
+    private static final int MAX = 100;
+    private static final String[] tasks = new String[MAX];
+    private static int n = 0;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -11,16 +14,33 @@ public class Bosh {
         System.out.println(line);
 
         while (true) {
-            String input = sc.nextLine();
+            String input = sc.nextLine().trim();
+
             if (input.equals("bye")) {
                 System.out.println(line);
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(line);
                 break;
+            } else if (input.equals("list")) {
+                System.out.println(line);
+                for (int i = 0; i < n; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(line);
+            } else if (!input.isEmpty()) {
+                if (n < MAX) {
+                    tasks[n++] = input;
+                    System.out.println(line);
+                    System.out.println(" added: " + input);
+                    System.out.println(line);
+                } else {
+                    System.out.println(line);
+                    System.out.println(" Sorry, the list is full (100 items).");
+                    System.out.println(line);
+                }
+            } else {
+                // Ignore empty lines
             }
-            System.out.println(line);
-            System.out.println("Your Message: " + input);
-            System.out.println(line);
         }
     }
 }
