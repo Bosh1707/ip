b75dc92fd660482e33b7d2d942f86890b8c13cd0
Joshua <joshua.rahul1707@gmail.com>
2025-08-20 16:45:49 +0800

    Level-0: greeting and immediate exit

---
+++ src/main/java/Bosh.java
@@ -0,0 +1,11 @@
+public class Bosh {
+    private static final String line = "____________________________________________________________";
+    public static void main(String[] args) {
+        System.out.println(line);
+        System.out.println(" Hello! I'm Bosh");
+        System.out.println(" What can I do for you?");
+        System.out.println(line);
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
