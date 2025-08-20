f98f022316b90c1894b0c6a946031e46715ca395
Joshua <joshua.rahul1707@gmail.com>
2025-08-20 17:32:24 +0800

    Level-1: Echo user input and exit on bye

--- src/main/java/Bosh.java
+++ src/main/java/Bosh.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
+
 public class Bosh {
     private static final String line = "____________________________________________________________";
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println(line);
         System.out.println(" Hello! I'm Bosh");
         System.out.println(" What can I do for you?");
         System.out.println(line);
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(line);
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(line);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+            System.out.println(line);
+            System.out.println("Your Message: " + input);
+            System.out.println(line);
+        }
     }
 }
